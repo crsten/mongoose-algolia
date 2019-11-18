0f81d8ec727ca16265dba61f8bc65027e760c58a
Carsten Jacobsen <car.jacobsen@gmail.com>
2019-11-18 19:06:14 +0100

    feat(indexName): added promise support for indexName function which opens up new possibilities for i
    fix #11

--- src/operations.js
+++ src/operations.js
@@ -1,70 +1,72 @@
-'use strict'
+"use strict";
 
-const utils = require('./utils')
-const clc = require('cli-color')
+const utils = require("./utils");
+const clc = require("cli-color");
 
 module.exports = function(options, client) {
-  this.pre('save', function(next) {
-    let isModified = false
+  this.pre("save", function(next) {
+    let isModified = false;
 
-    let relevantKeys = utils.GetRelevantKeys(this.toJSON(), options.selector)
+    let relevantKeys = utils.GetRelevantKeys(this.toJSON(), options.selector);
     if (relevantKeys && relevantKeys.length) {
       relevantKeys.forEach(key => {
-        if (this.isModified(key)) isModified = true
-      })
+        if (this.isModified(key)) isModified = true;
+      });
     } else {
-      if (this.isModified()) isModified = true
+      if (this.isModified()) isModified = true;
     }
 
-    this.wasNew = this.isNew
-    this.wasModified = isModified
-    next()
-  })
+    this.wasNew = this.isNew;
+    this.wasModified = isModified;
+    next();
+  });
 
-  this.post('save', function() {
-    let indices = utils.GetIndexName(this, options.indexName)
-    if (indices instanceof Array) {
-      indices.forEach(index => SyncItem(this, client.initIndex(index)))
-    } else {
-      SyncItem(this, client.initIndex(indices))
-    }
-  })
+  this.post("save", function() {
+    utils.GetIndexName(this, options.indexName).then(indices => {
+      if (indices instanceof Array) {
+        indices.forEach(index => SyncItem(this, client.initIndex(index)));
+      } else {
+        SyncItem(this, client.initIndex(indices));
+      }
+    });
+  });
 
-  this.post('remove', function() {
-    let indices = utils.GetIndexName(this, options.indexName)
-    if (indices instanceof Array) {
-      indices.forEach(index => RemoveItem(this, client.initIndex(index)))
-    } else {
-      RemoveItem(this, client.initIndex(indices))
-    }
-  })
+  this.post("remove", function() {
+    utils.GetIndexName(this, options.indexName).then(indices => {
+      if (indices instanceof Array) {
+        indices.forEach(index => RemoveItem(this, client.initIndex(index)));
+      } else {
+        RemoveItem(this, client.initIndex(indices));
+      }
+    });
+  });
 
   function RemoveItem(context, index) {
     index.deleteObject(context._id.toString(), err => {
       if (err)
         return console.error(
           clc.blackBright(`[${new Date().toLocaleTimeString()}]`),
-          clc.cyanBright('[Algolia-sync]'),
-          ' -> ',
-          clc.red.bold('Error'),
-          ' -> ',
-          err,
-        )
+          clc.cyanBright("[Algolia-sync]"),
+          " -> ",
+          clc.red.bold("Error"),
+          " -> ",
+          err
+        );
       if (options.debug)
         console.log(
           clc.blackBright(`[${new Date().toLocaleTimeString()}]`),
-          clc.cyanBright('[Algolia-sync]'),
-          ' -> ',
-          clc.greenBright('Deleted'),
-          ' -> ObjectId: ',
-          context._id,
-        )
-    })
+          clc.cyanBright("[Algolia-sync]"),
+          " -> ",
+          clc.greenBright("Deleted"),
+          " -> ObjectId: ",
+          context._id
+        );
+    });
   }
 
   function SyncItem(context, index) {
     if (options.filter && !options.filter(context._doc)) {
-      RemoveItem(context, index)
+      RemoveItem(context, index);
     } else if (context.wasNew) {
       utils
         .ApplyPopulation(context, options.populate)
@@ -73,50 +75,53 @@ module.exports = function(options, client) {
             populated.toObject({
               versionKey: false,
               transform: function(doc, ret) {
-                if (doc.constructor.modelName !== populated.constructor.modelName) return ret
+                if (
+                  doc.constructor.modelName !== populated.constructor.modelName
+                )
+                  return ret;
 
-                ret = utils.ApplyVirtuals(ret, options.virtuals)
-                ret = utils.ApplyMappings(ret, options.mappings)
-                ret = utils.ApplyDefaults(ret, options.defaults)
+                ret = utils.ApplyVirtuals(ret, options.virtuals);
+                ret = utils.ApplyMappings(ret, options.mappings);
+                ret = utils.ApplyDefaults(ret, options.defaults);
 
-                delete ret._id
+                delete ret._id;
 
-                return utils.ApplySelector(ret, options.selector)
-              },
+                return utils.ApplySelector(ret, options.selector);
+              }
             }),
             context._id,
             (err, content) => {
               if (err)
                 return console.error(
                   clc.blackBright(`[${new Date().toLocaleTimeString()}]`),
-                  clc.cyanBright('[Algolia-sync]'),
-                  ' -> ',
-                  clc.red.bold('Error'),
-                  ' -> ',
-                  err,
-                )
+                  clc.cyanBright("[Algolia-sync]"),
+                  " -> ",
+                  clc.red.bold("Error"),
+                  " -> ",
+                  err
+                );
               if (options.debug)
                 console.log(
                   clc.blackBright(`[${new Date().toLocaleTimeString()}]`),
-                  clc.cyanBright('[Algolia-sync]'),
-                  ' -> ',
-                  clc.greenBright('Created'),
-                  ' -> ObjectId: ',
-                  content.objectID,
-                )
-            },
-          )
+                  clc.cyanBright("[Algolia-sync]"),
+                  " -> ",
+                  clc.greenBright("Created"),
+                  " -> ObjectId: ",
+                  content.objectID
+                );
+            }
+          );
         })
         .catch(err => {
           console.error(
             clc.blackBright(`[${new Date().toLocaleTimeString()}]`),
-            clc.cyanBright('[Algolia-sync]'),
-            ' -> ',
-            clc.red.bold('Error (at population)'),
-            ' -> ',
-            err,
-          )
-        })
+            clc.cyanBright("[Algolia-sync]"),
+            " -> ",
+            clc.red.bold("Error (at population)"),
+            " -> ",
+            err
+          );
+        });
     } else if (context.wasModified) {
       utils
         .ApplyPopulation(context, options.populate)
@@ -125,51 +130,54 @@ module.exports = function(options, client) {
             populated.toObject({
               versionKey: false,
               transform: function(doc, ret) {
-                if (doc.constructor.modelName !== populated.constructor.modelName) return ret
+                if (
+                  doc.constructor.modelName !== populated.constructor.modelName
+                )
+                  return ret;
 
-                ret = utils.ApplyVirtuals(ret, options.virtuals)
-                ret = utils.ApplyMappings(ret, options.mappings)
-                ret = utils.ApplyDefaults(ret, options.defaults)
-                ret = utils.ApplySelector(ret, options.selector)
+                ret = utils.ApplyVirtuals(ret, options.virtuals);
+                ret = utils.ApplyMappings(ret, options.mappings);
+                ret = utils.ApplyDefaults(ret, options.defaults);
+                ret = utils.ApplySelector(ret, options.selector);
 
-                delete ret._id
-                ret.objectID = doc._id
+                delete ret._id;
+                ret.objectID = doc._id;
 
-                return ret
-              },
+                return ret;
+              }
             }),
             (err, content) => {
               if (err)
                 return console.error(
                   clc.blackBright(`[${new Date().toLocaleTimeString()}]`),
-                  clc.cyanBright('[Algolia-sync]'),
-                  ' -> ',
-                  clc.red.bold('Error'),
-                  ' -> ',
-                  err,
-                )
+                  clc.cyanBright("[Algolia-sync]"),
+                  " -> ",
+                  clc.red.bold("Error"),
+                  " -> ",
+                  err
+                );
               if (options.debug)
                 console.log(
                   clc.blackBright(`[${new Date().toLocaleTimeString()}]`),
-                  clc.cyanBright('[Algolia-sync]'),
-                  ' -> ',
-                  clc.greenBright('Updated'),
-                  ' -> ObjectId: ',
-                  content.objectID,
-                )
-            },
-          )
+                  clc.cyanBright("[Algolia-sync]"),
+                  " -> ",
+                  clc.greenBright("Updated"),
+                  " -> ObjectId: ",
+                  content.objectID
+                );
+            }
+          );
         })
         .catch(err => {
           console.error(
             clc.blackBright(`[${new Date().toLocaleTimeString()}]`),
-            clc.cyanBright('[Algolia-sync]'),
-            ' -> ',
-            clc.red.bold('Error (at population)'),
-            ' -> ',
-            err,
-          )
-        })
+            clc.cyanBright("[Algolia-sync]"),
+            " -> ",
+            clc.red.bold("Error (at population)"),
+            " -> ",
+            err
+          );
+        });
     }
   }
-}
+};
